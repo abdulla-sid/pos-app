5436a4f04355a3bd4e14f7500fa3462d5eb4e1a8
abdullah <abdullah.siddiqui@carbonteq.com>
2026-06-09 13:43:47 +0500

    feat: AntD v6 SSR registry + theme tokens

---
+++ src/app/providers.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { ConfigProvider } from "antd";
+import type { ReactNode } from "react";
+import { theme } from "@/shared/theme/tokens"
+
+
+export function Providers({ children }: { children: ReactNode }) {
+    return (
+        <ConfigProvider theme={theme}>
+            {children}
+        </ConfigProvider>
+    );
+}
